7bf5235e4c4aeee0c4d550a4de773fc4c93317e8
Marshall Whittaker <marshallwhittaker@gmail.com>
2021-05-31 05:48:05 -0400

    Update pcap_gen_testcases.py

--- tools/pcap_gen_testcases.py
+++ tools/pcap_gen_testcases.py
@@ -4,20 +4,17 @@ try:
     import scapy.all as scapy
 except ImportError:
     import scapy
-def parse_http_pcap(pcap_path):
+def parse_pcap(pcap_path):
     s = 0
     pcap_infos = list()
     packets = scapy.rdpcap(pcap_path)
     for p in packets:
-        print("----")
-        # To determine whether a layer is included, use haslayer
         if p.haslayer("IP"):
             src_ip = p["IP"].src
             dst_ip = p["IP"].dst
             print("sip: %s" % src_ip)
             print("dip: %s" % dst_ip)
         if p.haslayer("TCP"):
-            # Get the original load of a layer. Payload.original
             raw_d = p["TCP"].payload.original
             sport = p["TCP"].sport
             dport = p["TCP"].dport
@@ -27,8 +24,7 @@ def parse_http_pcap(pcap_path):
             print("dport: %s" % dport)
             print("raw_http:\n%s" % raw_d)
             num = str(s)
-            out = open(dport_dir + "/out." + num + ".txt", "wb")
+            out = open(dport_dir + "/pcap.data_packet" + num + ".dat", "wb")
             out.write(raw_d)
             s = s + 1
-
-parse_http_pcap("/home/marshall/capture.pcapng")
+parse_pcap("/home/marshall/capture.pcapng")
