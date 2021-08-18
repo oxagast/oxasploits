b0518817ff574643d46b154b994a38fbe5e69ce3
Marshall Lee Whittaker <marshallwhittaker@gmail.com>
2021-08-18 03:52:08 +0000

    Changed perms, and slightly modified all_suid.sh so it doens't dump in cur dir.

--- tools/all_suid.sh
+++ tools/all_suid.sh
@@ -1,5 +1,9 @@
+mkdir debtmp;
+cd debtmp;
 PKG=$(apt-cache search . | cut -f 1 -d ' ');
 echo $PKG | xargs apt-get download;
 DEB=(`find *.deb`);
 for i in ${DEB[@]};
 do dpkg -c $i | cut -c 4- | grep ^s | cut -f 2 -d '.' ; done | tee deb_suid_root.txt
+cd ..;
+rm -rf debtmp;
