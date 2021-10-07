76572aa86b968d9901f9e9c514923f5590f4cf1e
Marshall Whittaker <marshallwhittaker@gmail.com>
2021-10-06 20:24:07 -0400

    Updated so its more user friendly.

--- tools/radamsa_fuzzwin.ps1
+++ tools/radamsa_fuzzwin.ps1
@@ -1,18 +1,28 @@
-mkdir f;
+# edit app path and testcase files to reflect what you're fuzzing
+$apppath = "C:\Program Files\KMPlayer 64X\KMPlayer64.exe";
+$testcase = "sample.mp4";
+
+
+$ext = [System.IO.Path]::GetExtension($testcase)
+$appname = Split-Path $apppath -leaf
+if (!(Test-Path "f"))
+{
+  mkdir f;
+}
 :floop for (;;) {
   for ($i = 0; $i -le 9; $i++) {
-    .\radamsa.exe sample.mp4 -o f\"$i.mp4";
+    .\radamsa.exe sample$ext -o f\"$i$ext";
   }
-  $files = Get-ChildItem "C:\fuzz\f\*.mp4"
+  $files = Get-ChildItem ".\f\*$ext"
   foreach ($f in $files){
-    & 'C:\Program Files\VideoLAN\VLC\vlc.exe' $f;
+    & $apppath $f;
     $ret = $LastExitCode;
     $tf = $?;
     echo "$f Exit was: $ret : $tf";
     if ($tf -ne "True") {
       break floop;
     }
-    sleep 3;
-    Stop-Process -name "vlc"
+    sleep 6;
+    taskkill /im "$appname";
   }
 }
