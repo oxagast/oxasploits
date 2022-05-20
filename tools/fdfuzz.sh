e2cbff173ce0382acc4624979f25b3d5dd76aa11
Marshall Whittaker <marshallwhittaker@gmail.com>
2022-05-20 11:58:04 -0400

    Doesn't clobber itself now.  Also warning.

--- tools/fdfuzz.sh
+++ tools/fdfuzz.sh
@@ -1,3 +1,5 @@
+# File descriptor fuzzer
+# this will trash things around the system, do not run unless in a VM
 while [ 42 ]
 do
 rm open_fd fd
@@ -5,10 +7,10 @@ for i in $(ps aux | awk '//{print $2}')
 do
 find /proc/$i/fd/ >> open_fd 2>/dev/null
 done;
-for fd in $(cat open_fd | grep fd/[[:digit:]])
+for fd in $(cat open_fd | grep fd/[[:digit:]] | grep -v $$)
 do
-printf "hellofuzzed" > $fd
-printf "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" > $fd
+printf "\x00" > $fd
 printf "%s%s%s%s%s" > $fd
+dd if=/dev/urandom bs=4 count=256 > $fd
 done;
 done;
