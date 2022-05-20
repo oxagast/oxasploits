01bdc74888d824125d940eccc4312308e2145f3c
Marshall Whittaker <marshallwhittaker@gmail.com>
2022-05-20 12:12:00 -0400

    Formating changes.

--- tools/fdfuzz.sh
+++ tools/fdfuzz.sh
@@ -1,16 +1,14 @@
 # File descriptor fuzzer
 # this will trash things around the system, do not run unless in a VM
-while [ 42 ]
-do
-rm open_fd fd
-for i in $(ps aux | awk '//{print $2}')
-do
-find /proc/$i/fd/ >> open_fd 2>/dev/null
-done;
-for fd in $(cat open_fd | grep fd/[[:digit:]] | grep -v $$)
-do
-printf "\x00" > $fd
-printf "%s%s%s%s%s" > $fd
-dd if=/dev/urandom bs=4 count=256 > $fd
-done;
+while [ 42 ];
+  do rm open_fd fd;
+  for i in $(ps aux | awk '//{print $2}');
+    do find /proc/$i/fd/ >> open_fd 2>/dev/null;
+  done;
+  for fd in $(cat open_fd | grep fd/[[:digit:]] | grep -v $$);
+    do
+    printf "\x00" > $fd;
+    printf "%s%s%s%s%s" > $fd;
+    dd if=/dev/urandom bs=4 count=256 > $fd;
+  done;
 done;
