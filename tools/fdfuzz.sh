ef212bfc3010d8225b932ba9a2cd9e2eec969936
Marshall Whittaker <marshallwhittaker@gmail.com>
2022-05-21 10:18:35 -0400

    Updated so it can only fuzz one program.

--- tools/fdfuzz.sh
+++ tools/fdfuzz.sh
@@ -1,16 +1,21 @@
+#!/usr/bin/bash
 # Marshall Whittaker / oxagast
 # A small file descriptor fuzzer.
 # IMPORTANTL: This will trash things around the system, do not run unless it's contained in a VM.
 
-while [ 42 ];
-  do rm open_fd fd;
-  for i in $(ps aux | awk '//{print $2}');
-    do find /proc/$i/fd/ >> open_fd 2>/dev/null;
+pn=$1
+echo $pn;
+rm open_fd fd;
+while true;
+  do
+  for proc in $(ps aux | grep $pn | awk '//{print $2}');
+    do find /proc/$proc/fd/ >> open_fd 2>/dev/null;
   done;
   for fd in $(cat open_fd | grep fd/[[:digit:]] | grep -v $$);
     do
-    printf "\x00" > $fd;
     printf "%s%s%s%s%s" > $fd;
     dd if=/dev/urandom bs=4 count=256 > $fd;
+    if [[ $(pgrep -x $pn | wc -l) -lt 1 ]]; then rm open_fd fd; exit; fi;
   done;
 done;
+rm open_fd fd 2>/dev/null;
