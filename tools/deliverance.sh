daf94520e331145113dabd55678b7e6d91f36f37
Marshall Whittaker <marshallwhittaker@gmail.com>
2022-05-24 07:09:20 -0400

    Help

--- tools/deliverance.sh
+++ tools/deliverance.sh
@@ -7,7 +7,8 @@
 # noun
 #  1. the action of being rescued or set free.
 #
-# Useage: ./deliverance.sh firefox
+# Useage: ./deliverance.sh [progname] [buf size optional]
+# Useage: ./deliverance.sh firefox 512
 # Note: Some processes will spin off other processes, for example with skype, you need to use
 # skypeforlinux as the program name to fuzz, as that is how it shows up in ps/pgrep.
 
