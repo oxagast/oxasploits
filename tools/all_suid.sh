34d51a4d56db97ec54cc2eb238e01f06c5b6d8a8
Marshall Whittaker <marshall@oxasploits.com>
2025-07-16 15:24:10 -0400

    Removed some old things that were unnecessary.

--- tools/all_suid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Leaves a list of an entire repository's SUID binaries in deb_suid_root.txt.
-PKG=$(apt-cache search . | cut -f 1 -d ' ');
-for i in ${PKG[@]}; do
- apt-get download $i;
-dpkg -c *.deb | cut -c 4- | grep ^s | cut -f 2 -d '.' | tee -a deb_suid_root.txt
-rm *.deb
-done
--- tools/change_endianess.sh
+++
@@ -1,1 +0,0 @@
- hexdump -v -e '1/4 "%08x"' -e '"\n"' | xxd -r -p
